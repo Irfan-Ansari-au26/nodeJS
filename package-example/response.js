8a23713d2f82105683a196c4a67d5e88cc50c0dc
phoreka <irfan398649@gmail.com>
2022-08-06 16:28:20 +0530

    add nodemon globaly

--- package-example/response.js
+++ package-example/response.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-axios.get("https://www.google.com").then((response) => {
+axios.get("https://www.facebook.com").then((response) => {
   console.log("Google", response);
 })
 .catch(err=>console.log(err))
